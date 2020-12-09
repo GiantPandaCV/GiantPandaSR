0030a4f3ccf77c28b9b235b5c6e847a37d1fe28c
cydia <cydiachencc@icloud.com>
2020-12-09 23:48:33 +0800

    ad  all

---
+++ model/SRCNN.py
@@ -0,0 +1,27 @@
+import torch
+import torch.nn as nn
+
+"""
+Input_channel: 1
+
+Network Structure:
+CONV1 -> ReLU -> CONV2 -> ReLU -> CONV3 -> ReLU
+
+Network Loss:
+Euclidean Loss: Compare SR with GT
+"""
+
+
+class SRCNN(nn.Module):
+    def __init__(self, num_channels=1):
+        super(SRCNN, self).__init__()
+        self.conv1 = nn.Conv2d(num_channels, 64, kernel_size=9, padding=9 // 2)
+        self.conv2 = nn.Conv2d(64, 32, kernel_size=5, padding=5 // 2)
+        self.conv3 = nn.Conv2d(32, num_channels, kernel_size=5, padding=5 // 2)
+        self.relu = nn.ReLU(inplace=True)
+
+    def forward(self, x):
+        x = self.relu(self.conv1(x))
+        x = self.relu(self.conv2(x))
+        x = self.conv3(x)
+        return x
